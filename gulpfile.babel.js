3905852b26f9670b66d0f21f90ac8734bdeac90e
Joe Joiner <joejoinerr@gmail.com>
2020-07-17 16:26:57 +0100

    Remove Sass references in build config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -32,7 +32,6 @@ const paths = {
   dist: './dist/',
   html: '**/*.html',
   twig: '**/*.html.twig',
-  sass: 'sass/**/*.scss',
   css: 'css/**/*.css',
   js: 'js/**/*.js',
   img: 'img/**/*.+(png|jpg|svg|gif)',
@@ -74,19 +73,11 @@ function html() {
   #CSS
 \*------------------------------------*/
 
-// Compile Sass
+// Compile CSS
 
 function css() {
-  const sassOptions = {
-    errLogToConsole: true,
-    includePaths: ['./node_modules'],
-    outputStyle: 'expanded',
-    precision: 2
-  };
-
-  return src(paths.src + paths.sass)
+  return src(paths.src + paths.css)
     .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass(sassOptions).on('error', plugins.sass.logError))
     .pipe(plugins.postcss([
       tailwindcss('./tailwind.js'),
       autoprefixer()
@@ -94,20 +85,6 @@ function css() {
     .pipe(plugins.sourcemaps.write('./maps'))
     .pipe(dest(paths.dist + 'css/'))
     .pipe(browserSync.stream())
-};
-
-
-// Lint Sass
-
-function lint() {
-  const scssLintOptions = {
-    config: 'scss-lint.yml'
-  }
-
-  return src(paths.src + paths.sass)
-    .pipe(plugins.cached('plugins.scssLint'))
-    .pipe(plugins.scssLint(scssLintOptions))
-    .pipe(plugins.scssLint.failReporter('E'))
 };
 
 
@@ -204,7 +181,7 @@ export const serve = series(parallel(css, html, twig, img, font), function serve
     }
   });
 
-  watch(paths.src + paths.sass, css);
+  watch(paths.src + paths.css, css);
   watch(paths.src + paths.html, html);
   watch(paths.src + paths.twig, twig);
   watch(paths.dist + paths.html).on('change', bs.reload);
