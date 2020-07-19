0cd2c0183a218a8f0bf9163cee5ac0970e0e5972
Joe Joiner <joejoinerr@gmail.com>
2020-07-19 08:57:58 +0100

    Fix path issue in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -188,7 +188,7 @@ function startServer() {
 
   watch(paths.src + paths.css, compileCSS);
   watch(paths.src + paths.html, copyRootFiles);
-  watch(pathc.src + paths.img, copyImg);
+  watch(paths.src + paths.img, copyImg);
   watch(paths.dist + paths.html).on('change', bs.reload);
 };
 
